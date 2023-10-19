23a1aebfe24db8611452bfe1cbd6748967ee6b30
TaylorTurnerIT <tjturner44@tntech.edu>
2023-10-19 00:36:15 -0500

    A bit of cleanup

--- driver.py
+++ driver.py
@@ -106,40 +106,8 @@ while not exit:
             exit = True
         if event.type == pygame.USEREVENT:
             rect.sort()
-            
-    
     canvas.fill("#b3cde0")
     rect.draw(canvas)
     pygame.display.update()
-    
-    
-    
     # deltaTime in seconds.
     deltaTime = clock.tick(60) / 1000.0
-
-
-    """ https://en.wikipedia.org/wiki/Cocktail_shaker_sort 
-
-procedure cocktailShakerSort(A : list of sortable items) is
-    do
-        swapped := false
-        for each i in 0 to length(A) − 1 do:
-            if A[i] > A[i + 1] then // test whether the two elements are in the wrong order
-                swap(A[i], A[i + 1]) // let the two elements change places
-                swapped := true
-            end if
-        end for
-        if not swapped then
-            // we can exit the outer loop here if no swaps occurred.
-            break do-while loop
-        end if
-        swapped := false
-        for each i in length(A) − 1 to 0 do:
-            if A[i] > A[i + 1] then
-                swap(A[i], A[i + 1])
-                swapped := true
-            end if
-        end for
-    while swapped // if no elements have been swapped, then the list is sorted
-end procedure
-    """
